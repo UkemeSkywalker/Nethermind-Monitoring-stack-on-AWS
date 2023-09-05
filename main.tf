327ff00e695997c1870eab525ce589a885859d76
Ukeme David Eseme <skywalkershadow9@gmail.com>
2023-09-05 14:37:59 +0000

    initial payload

--- main.tf
+++ main.tf
@@ -1,45 +1,36 @@
-resource "digitalocean_ssh_key" "key" {
-  name       = "SSH Key Example for "
-  public_key = file("./private.key.pub")
-}
+resource "aws_instance" "nethermind-client" {
+  ami           = var.ami 
+  instance_type = var.instance_type
+  key_name      = var.key_name 
+  security_groups = ["default"]
 
-resource "digitalocean_droplet" "nethermind-client" {
-  count       = "${var.num}"
-  image       = "ubuntu-20-04-x64"
-  name        = "${format("%s-%s-%02d", var.prefix, "lon1", count.index + 1)}"
-  region      = "lon1"
-  size        = "${var.sizeList[var.size]}"
-  ssh_keys = [digitalocean_ssh_key.key.fingerprint]
+
   connection {
-    user        = "root"
-    type        = "ssh"
-    private_key = "${file(var.pvt_key)}"
-    timeout     = "2m"
-    host        = self.ipv4_address
+  type        = "ssh"
+  user        = "ubuntu" # or the appropriate username for your AMI
+  private_key = file("./Nethermind-Node.pem")
+  host        = self.public_ip
   }
 
-  provisioner "file" {
-    source      = "./docker-compose.yml"
-    destination = "/root/docker-compose.yaml"
-  }
-
-  provisioner "file" {
-    source      = "./NLog.config"
-    destination = "/root/NLog.config"
-  }
 
   provisioner "remote-exec" {
     inline = [
-      "sudo apt-get update",
-      "sudo apt-get install -y docker docker-compose",
-      "git clone https://github.com/NethermindEth/metrics-infrastructure.git",
-      "cp -r metrics-infrastructure/grafana metrics-infrastructure/prometheus .",
-      "export HOST=$(curl ifconfig.me)",
-      "export NAME=\"Nethermind node on ${var.config}\"",
-      "sed -i '10s/.*/            - NETHERMIND_CONFIG=${var.config}/' docker-compose.yaml",
-      "sed -i '11s/.*/            - NETHERMIND_JSONRPCCONFIG_ENABLED=${var.rpc_enabled}/' docker-compose.yaml",
-      "sed -i '36s/.*/            <target xsi:type=\"Seq\" serverUrl=\"'\"http:\\/\\/$HOST:5341\"'\" apiKey=\"Test\">/' NLog.config",
-      "docker-compose up -d"
-    ]
+    "sudo -E -n apt-get update && sudo -E -n apt-get install -y docker docker-compose",
+    "git clone https://github.com/NethermindEth/metrics-infrastructure.git",
+    "cp -r metrics-infrastructure/grafana metrics-infrastructure/prometheus .",
+    "export HOST=$(curl ifconfig.me)",
+    "export NAME=\"Nethermind node on ${var.config}\"",
+    "sed -i '10s/.*/            - NETHERMIND_CONFIG=${var.config}/' docker-compose.yaml",
+    "sed -i '11s/.*/            - NETHERMIND_JSONRPCCONFIG_ENABLED=${var.rpc_enabled}/' docker-compose.yaml",
+    "sed -i '36s/.*/            <target xsi:type=\"Seq\" serverUrl=\"'\"http:\\/\\/$HOST:5341\"'\" apiKey=\"Test\">/' NLog.config",
+    "sudo -E -n mv ./docker-compose.yaml /root/docker-compose.yaml",
+    "sudo -E -n chown root:root /root/docker-compose.yaml",
+    "sudo -E -n mv ./NLog.config /root/NLog.config",
+    "sudo -E -n chown root:root /root/NLog.config",
+    "sudo -E -n docker-compose up -d",
+    "if [ $? -eq 0 ]; then echo 'Provisioning successful'; else echo 'Provisioning failed'; fi"
+  ]
+
+
   }
 }
--- variables.tf
+++ variables.tf
@@ -1,14 +1,34 @@
-variable "num" {
-  description = "Number of VM's with preconfigured Nethermind Docker environment to create"
-}
+# variable "num" {
+#   description = "Number of VM's with preconfigured Nethermind Docker environment to create"
+# }
 
-variable "pvt_key" {}
+# variable "pvt_key" {}
 
 variable "prefix" {
   description = "Instance prefix, this will be the basename of the host (e.g. <instance_name>-<instance-region>-<count>v)"
-  type = "string"
+  type = string
   default = "nethermind"
 }
+
+variable "key_name" {
+  description = "Instance Key name"
+  type = string
+  default = "Nethermind-Node"
+}
+
+variable "instance_type" {
+  description = "EC2 instance type"
+  type = string
+  default = "c5.2xlarge"
+}
+
+variable "ami" {
+  description = "EC2 instance Amazon Machine Image value"
+  type = string
+  default = "ami-0261755bbcb8c4a84"
+}
+
+
 
 # variable "client_count" {
 #   description = "Number of Nethermind clients which will be running on each VM (e.g. --scale nethermind=5)"
@@ -25,36 +45,41 @@ variable "rpc_enabled" {
   type = bool
 }
 
-variable "sizeList" {
-  type = "map"
-  default = {
-      "s-1vcpu-2gb" = "s-1vcpu-2gb"
-      "s-2vcpu-2gb" = "s-2vcpu-2gb"
-      "s-2vcpu-4gb" = "s-2vcpu-4gb"
-      "s-4vcpu-8gb" = "s-4vcpu-8gb"
-      "s-6vcpu-16gb" = "s-6vcpu-16gb"
-      "s-8vcpu-32gb" = "s-8vcpu-32gb"
-      "s-32vcpu-192gb" = "s-32vcpu-192gb"
-  }
-}
+# variable "sizeList" {
+#   type = map
+#   default = {
+#       "s-1vcpu-2gb" = "s-1vcpu-2gb"
+#       "s-2vcpu-2gb" = "s-2vcpu-2gb"
+#       "s-2vcpu-4gb" = "s-2vcpu-4gb"
+#       "s-4vcpu-8gb" = "s-4vcpu-8gb"
+#       "s-6vcpu-16gb" = "s-6vcpu-16gb"
+#       "s-8vcpu-32gb" = "s-8vcpu-32gb"
+#       "s-32vcpu-192gb" = "s-32vcpu-192gb"
+#   }
+# }
 
-variable "size" {
-  description = <<EOT
-"Choose VM size you wish to deploy e.g. s-2vcpu-2gb
-List of mappings is displayed below:
-s-1vcpu-2gb = 10USD
-s-2vcpu-2gb = 15USD
-s-2vcpu-4gb = 20USD
-s-4vcpu-8gb = 40USD
-s-6vcpu-16gb = 80USD
-s-8vcpu-32gb = 160USD
-s-32vcpu-192gb = 960USD"
-EOT
-  type = string
-}
+# variable "size" {
+#   description = <<EOT
+# "Choose VM size you wish to deploy e.g. s-2vcpu-2gb
+# List of mappings is displayed below:
+# s-1vcpu-2gb = 10USD
+# s-2vcpu-2gb = 15USD
+# s-2vcpu-4gb = 20USD
+# s-4vcpu-8gb = 40USD
+# s-6vcpu-16gb = 80USD
+# s-8vcpu-32gb = 160USD
+# s-32vcpu-192gb = 960USD"
+# EOT
+#   type = string
+# }
 
-variable "do_token" {}
+# variable "do_token" {}
 
-provider "digitalocean" {
-  token = "${var.do_token}"
+# provider "digitalocean" {
+#   token = "${var.do_token}"
+# }
+
+provider "aws" {
+  region = "us-east-1" # Change to your desired region
+  
 }
