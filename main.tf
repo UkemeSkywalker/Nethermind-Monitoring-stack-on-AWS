9ee99711b0107b63140a495fdeaeab0a0779b8ef
Ukeme David Eseme <skywalkershadow9@gmail.com>
2023-09-13 21:06:16 +0000

    modify remote-exec

--- main.tf
+++ main.tf
@@ -12,25 +12,28 @@ resource "aws_instance" "nethermind-client" {
   host        = self.public_ip
   }
 
+provisioner "file" {
+    source      = "./docker-compose.yml"
+    destination = "/home/ubuntu/docker-compose.yaml"
+  }
+
+  provisioner "file" {
+    source      = "./NLog.config"
+    destination = "/home/ubuntu/NLog.config"
+  }
 
   provisioner "remote-exec" {
     inline = [
-    "sudo -E -n apt-get update && sudo -E -n apt-get install -y docker docker-compose",
-    "git clone https://github.com/NethermindEth/metrics-infrastructure.git",
-    "cp -r metrics-infrastructure/grafana metrics-infrastructure/prometheus .",
-    "export HOST=$(curl ifconfig.me)",
-    "export NAME=\"Nethermind node on ${var.config}\"",
-    "sed -i '10s/.*/            - NETHERMIND_CONFIG=${var.config}/' docker-compose.yaml",
-    "sed -i '11s/.*/            - NETHERMIND_JSONRPCCONFIG_ENABLED=${var.rpc_enabled}/' docker-compose.yaml",
-    "sed -i '36s/.*/            <target xsi:type=\"Seq\" serverUrl=\"'\"http:\\/\\/$HOST:5341\"'\" apiKey=\"Test\">/' NLog.config",
-    "sudo -E -n mv ./docker-compose.yaml /root/docker-compose.yaml",
-    "sudo -E -n chown root:root /root/docker-compose.yaml",
-    "sudo -E -n mv ./NLog.config /root/NLog.config",
-    "sudo -E -n chown root:root /root/NLog.config",
-    "sudo -E -n docker-compose up -d",
-    "if [ $? -eq 0 ]; then echo 'Provisioning successful'; else echo 'Provisioning failed'; fi"
-  ]
-
-
+      "sudo apt-get update",
+      "sudo apt-get install -y docker.io docker-compose git",
+      "git clone https://github.com/NethermindEth/metrics-infrastructure.git",
+      "cp -r metrics-infrastructure/grafana metrics-infrastructure/prometheus .",
+      "export HOST=$(curl ifconfig.me)",
+      "export NAME=\"Nethermind node on ${var.config}\"",
+      "sed -i '10s/.*/            - NETHERMIND_CONFIG=${var.config}/' docker-compose.yaml",
+      "sed -i '11s/.*/            - NETHERMIND_JSONRPCCONFIG_ENABLED=${var.rpc_enabled}/' docker-compose.yaml",
+      "sed -i '36s/.*/            <target xsi:type=\"Seq\" serverUrl=\"'\"http:\\/\\/$HOST:5341\"'\" apiKey=\"Test\">/' NLog.config",
+      "sudo docker-compose up -d"
+    ]
   }
 }
